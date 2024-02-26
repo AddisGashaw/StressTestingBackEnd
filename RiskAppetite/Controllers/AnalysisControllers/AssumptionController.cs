1527f5b358b44330a19c67331c3930eb93b3d00a
Gedewon F <gedigift@gmail.com>
2024-02-26 10:16:05 +0300

    assumption endpoint added

---
+++ RiskAppetite/Controllers/AnalysisControllers/AssumptionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RiskAppetite.Models;
+using RiskAppetite.Services.AnalysisServices.Assumption;
+
+
+
+namespace RiskAppetite.Controllers.AnalysisControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AssumptionController : ControllerBase
+    {
+        private readonly IAssumption _Assumption;
+
+        public AssumptionController(IAssumption assumption)
+        {
+            _Assumption = assumption;
+        }
+        [HttpGet("GetsdbforCredit")]
+        public async Task<ActionResult<List<Sdbcredit>>> GetSdbcredit([FromQuery] string year, [FromQuery] string quarter)
+        {
+            var result = await _Assumption.GetSdbcredit(year, quarter);
+            return Ok(result);
+        }
+        [HttpGet("GetsdbLiquidity")]
+        public async Task<ActionResult<List<Sdbliquidity>>> GetSdbLiquidity([FromQuery] string year, [FromQuery] string quarter)
+        {
+            var result = await _Assumption.GetSdbLiquidity(year, quarter);
+            return Ok(result);
+        }
+        [HttpGet("GetsdbMarket")]
+        public async Task<ActionResult<List<MarketRisk>>> GetsdbMarket([FromQuery] string year, [FromQuery] string quarter)
+        {
+            var result = await _Assumption.GetSdbMarket(year, quarter);
+            return Ok(result);
+        }
+        [HttpGet("GetsdbOperational")]
+        public async Task<ActionResult<List<OprationalRisk>>> GetsdbOperational([FromQuery] string year, [FromQuery] string quarter)
+        {
+            var result = await _Assumption.GetSdbOperational(year, quarter);
+            return Ok(result);
+        }
+
+    }
+}
+
---
+++ RiskAppetite/Services/AnalysisServices/Assumption/IAssumption.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RiskAppetite.Models;
+
+namespace RiskAppetite.Services.AnalysisServices.Assumption
+{
+    public interface IAssumption
+    {
+        Task<ActionResult<List<Sdbcredit>>> GetSdbcredit(string year, string quarter);
+        Task<ActionResult<List<Sdbliquidity>>> GetSdbLiquidity(string year, string quarter);
+        Task<ActionResult<List<MarketRisk>>> GetSdbMarket(string year, string quarter);
+        Task<ActionResult<List<OprationalRisk>>> GetSdbOperational(string year, string quarter);
+        
+    }
+}
