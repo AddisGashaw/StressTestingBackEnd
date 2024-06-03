07ee2554d45fb6f10812156c655a5486e30a9746
Gedewon F <gedigift@gmail.com>
2024-06-03 13:25:28 +0300

    end point for share and ratio implemented

---
+++ RiskAppetite/Controllers/AnalysisControllers/ShareAndGrowthForSEB.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RiskAppetite.Models;
+using RiskAppetite.Services.AnalysisServices.ShareAndGrowth;
+
+namespace RiskAppetite.Controllers.AnalysisControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ShareAndGrowthForSEB : ControllerBase
+    {
+        private readonly IShareAndGrowth_SEB _shareAndGrowth;
+
+        public ShareAndGrowthForSEB(IShareAndGrowth_SEB shareAndGrowth)
+        {
+            _shareAndGrowth = shareAndGrowth;
+        }
+        [HttpGet("GetsebforCredit")]
+        public async Task<ActionResult<List<SebcreditRisk>>> GetSebCredit([FromQuery] string[] years)
+        {
+            var result = await _shareAndGrowth.GetSebCredit(years);
+            return Ok(result);
+        }
+        [HttpGet("GetsebLiquidity")]
+        public async Task<ActionResult<List<SebliquidityRisk>>> GetSebLiquidity([FromQuery] string[] years)
+        {
+            var result = await _shareAndGrowth.GetSebLiquidity(years);
+            return Ok(result);
+        }
+        [HttpGet("GetsebMarket")]
+        public async Task<ActionResult<List<SebmarketRisk>>> GetsebMarket([FromQuery] string[] years)
+        {
+            var result = await _shareAndGrowth.GetSebMarket(years);
+            return Ok(result);
+        }
+
+        [HttpGet("GetsebMacroeconomics")]
+        public async Task<ActionResult<List<Sebmacroeconomic>>> GetsebMacroeconomics([FromQuery] string[] years)
+        {
+            var result = await _shareAndGrowth.GetSebMacroeconomics(years);
+            return Ok(result);
+        }
+
+    }
+}
+
